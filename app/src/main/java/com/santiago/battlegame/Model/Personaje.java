5135d8adaa76eec2462ebe22b81eb8d44a5c8415
santiago <santiagocernadas29@gmail.com>
2023-05-24 15:26:30 -0300

    se agrega el modelo del personaje

---
+++ app/src/main/java/com/santiago/battlegame/Model/Personaje.java
@@ -0,0 +1,67 @@
+package com.santiago.battlegame.Model;
+
+public class Personaje {
+    private Long id;
+    private String nombre;
+    private Integer fuerza;
+    private Integer velocidad;
+    private Integer agilidad;
+    private Integer resistencia;
+    private Integer inteligencia;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(Integer fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(Integer velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Integer getAgilidad() {
+        return agilidad;
+    }
+
+    public void setAgilidad(Integer agilidad) {
+        this.agilidad = agilidad;
+    }
+
+    public Integer getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(Integer resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public Integer getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(Integer inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+}
